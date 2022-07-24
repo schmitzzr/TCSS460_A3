f450cf0da20ab8d3b1f6634bb92149ca66f29d23
Zachary Schmitz <schmitzzr@yahoo.com>
2022-07-23 18:56:00 -0700

    final changes

--- app.js
+++ app.js
@@ -1,6 +1,73 @@
 // Assignment 3 REST Service
 // Zach Schmitz
-
 // import required module
 var express = require("express");
 var app = express();
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.status(200);
+    res.send("<h1>this REST service will convert numbers to and from Binary/Hexadecimal</h1>");
+    console.log("a request has been processed in / (root)");
+});
+
+// converting decimal numbers to binary
+app.get("/toBinary/:num", function(req, res) {
+    const num = parseInt(req.params.num);
+    if (num === undefined || isNaN(num) || num < 0) {
+        res.status(400);
+        res.json({error: "bad request"});
+        return ;
+    }
+    const result = num.toString(2);
+    console.log("/toBinary/num   request is made...");
+    res.json({"result" : result});
+});
+
+// converting binary numbers to decimal
+app.get("/fromBinary/:num", function(req, res) {
+    var format = /^[0-1]+$/;
+    const num = req.params.num;
+    if (num === undefined || isNaN(num) || !format.test(num)) {
+        res.status(400);
+        res.json({error: "bad request"});
+        return ;
+    }
+    const result = parseInt(num, 2);
+    console.log("/fromBinary/num   request is made...");
+    res.json({"result" : result});
+});
+
+// converting decimal numbers to hexadecimal
+app.get("/toHex/:num", function(req, res) {
+    const num = parseInt(req.params.num);
+    if (num === undefined || isNaN(num) || num < 0) {
+        res.status(400);
+        res.json({error: "bad request"});
+        return ;
+    }
+    const result = num.toString(16);
+    console.log("/toHex/num   request is made...");
+    res.json({"result" : result});
+});
+
+// converting hexadecimal numbers to decimal
+app.get("/fromHex/:num", function(req, res) {
+    var format = /^[0-9a-fA-F]+$/;
+    const num = req.params.num;
+    if (num === undefined || !format.test(num)) {
+        res.status(400);
+        res.json({error: "bad request"});
+        return ;
+    }
+    const result = parseInt(num, 16);
+    console.log("/fromHex/num   request is made...");
+    console.log(num);
+    res.json({"result" : result});
+});
+
+
+app.listen(3000, function () {
+    console.log("API version 1.0.0 is running on port 3000");
+});
